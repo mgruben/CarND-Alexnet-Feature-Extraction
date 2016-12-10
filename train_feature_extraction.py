74ee0919350fe54ce6013c0053dbed48ebf816f4
mgruben <GrubenM@gmail.com>
2016-12-10 17:22:11 -0600

    Define loss, training, and accuracy ops

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -58,5 +58,14 @@ probs = tf.nn.softmax(logits)
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
+loss_op = tf.reduce_mean(cross_entropy)
+opt = tf.train.AdamOptimizer()
+train_op = opt.minimize(loss_op, var_list=[fc8W, fc8b])
+init_op = tf.initialize_all_variables()
+
+preds = tf.arg_max(logits, 1)
+accuracy_op = tf.reduce_mean(tf.cast(tf.equal(preds, labels), tf.float32))
+
 
 # TODO: Train and evaluate the feature extraction model.
